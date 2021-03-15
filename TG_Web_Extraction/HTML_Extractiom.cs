8d20eb7c0db54686967636b64cf50456a638e056
Tushar <t29gupta@live.com>
2021-03-15 23:55:19 +0530

    Refactoring and beutifying

--- TG_Web_Extraction/HTML_Extractiom.cs
+++ TG_Web_Extraction/HTML_Extractiom.cs
@@ -13,37 +13,46 @@ namespace TG_Web_Extraction
 {
     public class HTML_Extractiom
     {
-        public string ReadHtmlFile()
+        public (string, string) ReadHtmlFile(string filePath)
         {
             try
             {
-                //bool validFile = false;
-                var fileLocation = @"E:\Cloud Drives\OneDrive\Documents\HQ plus - Backend Development Assignment\HQ plus - Backend Development Assignment\Task 1\Kempinski Hotel Bristol Berlin, Germany - Booking.com.html";
+                bool validFile = false;
+                var fileLocation = filePath;
 
-                //while (!validFile)
+                while (!validFile)
+                {
+                    Console.WriteLine("Enter the location of the html file...");
+                    Console.WriteLine($@"e.g. <Drive:>\<Folder>\{Path.GetFileName(filePath)}");
+                    fileLocation = Console.ReadLine();
+
+                    if (Path.GetExtension(fileLocation) == ".html" && File.Exists(fileLocation))
+                    {
+                        validFile = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("File not found or invalid file name entered");
+                    }
+                }
+
+                //using (var fileStream = File.OpenRead(fileLocation))
                 //{
-                //    Console.WriteLine("Enter the location of the html file...");
-                //    fileLocation = Console.ReadLine();
-
-                //    if (Path.GetExtension(fileLocation) == ".html" && File.Exists(fileLocation))
-                //    {
-                //        validFile = true;
-                //    }
-                //    else
-                //    {
-                //        Console.WriteLine("File not found or invalid file name entered");
-                //    }
+                //    GetHtmlDocumentFromStream(fileStream);
                 //}
 
-                return File.ReadAllText(fileLocation);
+                var currentFolder = Path.GetDirectoryName(fileLocation);
+
+                return (File.ReadAllText(fileLocation), currentFolder);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                return ("", "");
             }
-
-            return "";
         }
+
+        #region Fields
 
         const string HoltelName_Id = "hp_hotel_name";
         const string Address_Id = "hp_address_subtitle";
@@ -72,156 +81,203 @@ namespace TG_Web_Extraction
         const string Alt_hotel_row_RatingText_class = "js--hp-scorecard-scoreword";
         const string Alt_hotel_row_RatingPoint_class = "average";
 
-        public void Extractinfo(string htmlContent)
+        #endregion
+
+        public string Extractinfo(string htmlContent)
         {
             try
             {
                 BookingModel bmResult = new BookingModel();
 
-                var html = new HtmlDocument();
-                html.LoadHtml(htmlContent);
+                HtmlDocument html = GetHtmlDocumentFromStringContent(htmlContent);
 
-                // Getting Hotel Name
-                var hotelNameElement = html.GetElementbyId(HoltelName_Id);
-                if (hotelNameElement == null)
+                ExtractHotelName(bmResult, html);
+
+                ExtractHotelAddress(bmResult, html);
+
+                string reviewBest = ExtractHotelReviewsData(bmResult, html);
+
+                ExtractHotelDescription(bmResult, html);
+
+                ExtractHotelRoomCategories(bmResult, html);
+
+                ExtractAlternativeHotels(bmResult, html, reviewBest);
+
+                string resJson = JsonSerializer.Serialize(bmResult, new JsonSerializerOptions
                 {
-                    Console.WriteLine("Hotel name not found");
-                }
-                Console.WriteLine($"element : {hotelNameElement}\nName : {hotelNameElement?.InnerText}");
-                bmResult.HotelName = hotelNameElement?.InnerText.Replace("\n", "");
+                    WriteIndented = true
+                });
 
-                // Getting Hotel Address
-                var hotelAddressElement = html.GetElementbyId(Address_Id);
-                if (hotelAddressElement == null)
-                {
-                    Console.WriteLine("Hotel address not found");
-                }
-                Console.WriteLine($"element : {hotelAddressElement}\nAddress : {hotelAddressElement?.InnerText}");
-                bmResult.Address = hotelAddressElement?.InnerText.Replace("\n", "");
-
-                // Getting Hotel ReviewPoints and no of reviews
-                var reviewElement = html.GetElementbyId(Review_points_Block_Id);
-                var reviewDecendents = reviewElement?.Descendants("span").ToList();
-
-                var reviewText = reviewDecendents.FirstOrDefault(x => x.HasClass(Review_points_Text_Class))?.InnerText.Replace("\n", "");
-                var reviewscore = reviewDecendents.FirstOrDefault(x => x.HasClass(Review_points_Score_Class))?.InnerText.Replace("\n", "");
-                var reviewBest = reviewDecendents.FirstOrDefault(x => x.HasClass(Review_points_OutOf_Class))?.InnerText.Replace("\n", "");
-                var reviewCount = reviewDecendents.FirstOrDefault(x => x.HasClass(Number_of_reviews_Parent))?.ChildNodes.FirstOrDefault(c => c.HasClass(Number_of_reviews))?.InnerText.Replace("\n", "");
-
-                foreach (var item in reviewDecendents)
-                {
-                    Console.WriteLine(item.GetClasses().FirstOrDefault());
-                }
-
-                bmResult.ReviewPoint = new ReviewPoint
-                {
-                    Text = reviewText,
-                    Score = $"{reviewscore}/{reviewBest}"
-                };
-
-                bmResult.NoOfReviews = reviewCount;
-
-                // Getting description
-                var descElement = html.GetElementbyId(Description_Id);
-                var descDecendents = descElement.ParentNode.Descendants("p").ToList();// descElement?.Descendants("p").ToList();
-
-                StringBuilder sbDesc = new StringBuilder();
-                foreach (var para in descDecendents)
-                {
-                    var tempPara = para.InnerText.Replace("\n", "");
-
-                    if (!string.IsNullOrEmpty(tempPara))
-                    {
-                        sbDesc.AppendLine(para.InnerText);
-                    }
-                }
-
-                bmResult.Description = sbDesc.ToString();
-
-                // Getting room categories
-                var roomCategoriesElement = html.GetElementbyId(Room_Categories_Table_Id);
-                var roomCategoriesRows = roomCategoriesElement
-                    .Descendants("tr")
-                    .Select(i =>
-                        i.Descendants("td")
-                        .Where(c => c.HasClass(Room_Category_ToolTip) || c.HasClass(Room_Categories_row_class))
-                        .ToList())
-                    .Where(d => d.Any())
-                    .ToList();
-                //roomCategoriesElement.Descendants("td")?.Where(x => x.HasClass(Room_Categories_row_class)).ToList();// descElement?.Descendants("p").ToList();
-
-                bmResult.RoomCategories = new List<RoomCategories>();
-
-                foreach (var roomCat in roomCategoriesRows)
-                {
-                    var toolTip = roomCat[0].ChildNodes.Select(x => x.GetAttributeValue("title", "")).FirstOrDefault(t => !string.IsNullOrEmpty(t));
-                    var roomCategoryName = roomCat[1].InnerText.Replace("\n", "");
-
-                    bmResult.RoomCategories.Add(new RoomCategories
-                    {
-                        Description = toolTip,
-                        TypeName = roomCategoryName
-                    });
-                }
-
-                // Alternative Hotels
-                var altHotelsElement = html.GetElementbyId(Alternative_hotels_Table_Id);
-                var altHotelCollection = altHotelsElement.Descendants("td")
-                    .Select(p =>
-                         p.Descendants()
-                        .Where(z =>
-                                    z.HasClass(Alt_hotel_row_name_class) ||
-                                    z.HasClass(Alt_hotel_row_RatingText_class) ||
-                                    z.HasClass(Alt_hotel_row_RatingPoint_class) ||
-                                    z.HasClass(Alt_hotel_row_Desc_class))
-                        .Select(s => new
-                        {
-                            className = s.GetClasses().FirstOrDefault(),
-                            text = s.InnerText
-                        }).ToList()
-                    );
-
-                bmResult.AlternativeHotels = new List<AltHotels>();
-
-                foreach (var hotelRow in altHotelCollection)
-                {
-                    var altHotel = new AltHotels();
-                    altHotel.AltHotelRaiting = new ReviewPoint();
-                    foreach (var hotelRowElement in hotelRow)
-                    {
-                        switch (hotelRowElement.className)
-                        {
-                            case Alt_hotel_row_name_class:
-                                {
-                                    var hName = hotelRowElement.text.Split("\n").Where(s => !string.IsNullOrEmpty(s)).ToArray();
-
-                                    altHotel.AltHotelName = hName[0];
-                                    altHotel.AltHotelToolTip = hName[1];
-                                }
-                                break;
-                            case Alt_hotel_row_Desc_class:
-                                altHotel.AltHotelDesc = hotelRowElement.text.Replace("\n", "");
-                                break;
-                            case Alt_hotel_row_RatingText_class:
-                                altHotel.AltHotelRaiting.Text = hotelRowElement.text.Replace("\n", "");
-                                break;
-                            case Alt_hotel_row_RatingPoint_class:
-                                altHotel.AltHotelRaiting.Score = hotelRowElement.text.Replace("\n", "") + "/" + reviewBest;
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-
-                    bmResult.AlternativeHotels.Add(altHotel);
-                }
-
-                string resJson = JsonSerializer.Serialize(bmResult);
+                return resJson;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                return "";
             }
+        }
+
+        private static HtmlDocument GetHtmlDocumentFromStream(Stream htmlfileStream)
+        {
+            var html = new HtmlDocument();
+            html.Load(htmlfileStream);
+            return html;
+        }
+
+        private static HtmlDocument GetHtmlDocumentFromStringContent(string htmlContent)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(htmlContent);
+            return html;
+        }
+
+        private static void ExtractAlternativeHotels(BookingModel bmResult, HtmlDocument html, string reviewBest)
+        {
+            // Alternative Hotels
+            var altHotelsElement = html.GetElementbyId(Alternative_hotels_Table_Id);
+            var altHotelCollection = altHotelsElement.Descendants("td")
+                .Select(p =>
+                     p.Descendants()
+                    .Where(z =>
+                                z.HasClass(Alt_hotel_row_name_class) ||
+                                z.HasClass(Alt_hotel_row_RatingText_class) ||
+                                z.HasClass(Alt_hotel_row_RatingPoint_class) ||
+                                z.HasClass(Alt_hotel_row_Desc_class))
+                    .Select(s => new
+                    {
+                        className = s.GetClasses().FirstOrDefault(),
+                        text = s.InnerText
+                    }).ToList()
+                );
+
+            bmResult.AlternativeHotels = new List<AltHotels>();
+
+            foreach (var hotelRow in altHotelCollection)
+            {
+                var altHotel = new AltHotels();
+                altHotel.AltHotelRaiting = new ReviewPoint();
+                foreach (var hotelRowElement in hotelRow)
+                {
+                    switch (hotelRowElement.className)
+                    {
+                        case Alt_hotel_row_name_class:
+                            {
+                                var hName = hotelRowElement.text.Split("\n").Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+                                altHotel.AltHotelName = hName[0];
+                                altHotel.AltHotelToolTip = hName[1];
+                            }
+                            break;
+                        case Alt_hotel_row_Desc_class:
+                            altHotel.AltHotelDesc = hotelRowElement.text.Replace("\n", "");
+                            break;
+                        case Alt_hotel_row_RatingText_class:
+                            altHotel.AltHotelRaiting.Text = hotelRowElement.text.Replace("\n", "");
+                            break;
+                        case Alt_hotel_row_RatingPoint_class:
+                            altHotel.AltHotelRaiting.Score = hotelRowElement.text.Replace("\n", "") + "/" + reviewBest;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                bmResult.AlternativeHotels.Add(altHotel);
+            }
+        }
+
+        private static void ExtractHotelRoomCategories(BookingModel bmResult, HtmlDocument html)
+        {
+            // Getting room categories
+            var roomCategoriesElement = html.GetElementbyId(Room_Categories_Table_Id);
+            var roomCategoriesRows = roomCategoriesElement
+                .Descendants("tr")
+                .Select(i =>
+                    i.Descendants("td")
+                    .Where(c => c.HasClass(Room_Category_ToolTip) || c.HasClass(Room_Categories_row_class))
+                    .ToList())
+                .Where(d => d.Any())
+                .ToList();
+
+            bmResult.RoomCategories = new List<RoomCategories>();
+
+            foreach (var roomCat in roomCategoriesRows)
+            {
+                var toolTip = roomCat[0].ChildNodes.Select(x => x.GetAttributeValue("title", "")).FirstOrDefault(t => !string.IsNullOrEmpty(t));
+                var roomCategoryName = roomCat[1].InnerText.Replace("\n", "");
+
+                bmResult.RoomCategories.Add(new RoomCategories
+                {
+                    Description = toolTip,
+                    TypeName = roomCategoryName
+                });
+            }
+        }
+
+        private static void ExtractHotelDescription(BookingModel bmResult, HtmlDocument html)
+        {
+            // Getting description
+            var descElement = html.GetElementbyId(Description_Id);
+            var descDecendents = descElement.ParentNode.Descendants("p").ToList();// descElement?.Descendants("p").ToList();
+
+            StringBuilder sbDesc = new StringBuilder();
+            foreach (var para in descDecendents)
+            {
+                var tempPara = para.InnerText.Replace("\n", "");
+
+                if (!string.IsNullOrEmpty(tempPara))
+                {
+                    sbDesc.AppendLine(para.InnerText);
+                }
+            }
+
+            bmResult.Description = sbDesc.ToString();
+        }
+
+        private static string ExtractHotelReviewsData(BookingModel bmResult, HtmlDocument html)
+        {
+
+            // Getting Hotel ReviewPoints and no of reviews
+            var reviewElement = html.GetElementbyId(Review_points_Block_Id);
+            var reviewDecendents = reviewElement?.Descendants("span").ToList();
+
+            var reviewText = reviewDecendents.FirstOrDefault(x => x.HasClass(Review_points_Text_Class))?.InnerText.Replace("\n", "");
+            var reviewscore = reviewDecendents.FirstOrDefault(x => x.HasClass(Review_points_Score_Class))?.InnerText.Replace("\n", "");
+            var reviewBest = reviewDecendents.FirstOrDefault(x => x.HasClass(Review_points_OutOf_Class))?.InnerText.Replace("\n", "");
+            var reviewCount = reviewDecendents.FirstOrDefault(x => x.HasClass(Number_of_reviews_Parent))?.ChildNodes.FirstOrDefault(c => c.HasClass(Number_of_reviews))?.InnerText.Replace("\n", "");
+
+            bmResult.ReviewPoint = new ReviewPoint
+            {
+                Text = reviewText,
+                Score = $"{reviewscore}/{reviewBest}"
+            };
+
+            bmResult.NoOfReviews = reviewCount;
+            return reviewBest;
+        }
+
+        private static void ExtractHotelAddress(BookingModel bmResult, HtmlDocument html)
+        {
+            // Getting Hotel Address
+            var hotelAddressElement = html.GetElementbyId(Address_Id);
+            if (hotelAddressElement == null)
+            {
+                Console.WriteLine("Hotel address not found");
+            }
+
+            bmResult.Address = hotelAddressElement?.InnerText.Replace("\n", "");
+        }
+
+        private static void ExtractHotelName(BookingModel bmResult, HtmlDocument html)
+        {
+            // Getting Hotel Name
+            var hotelNameElement = html.GetElementbyId(HoltelName_Id);
+            if (hotelNameElement == null)
+            {
+                Console.WriteLine("Hotel name not found");
+            }
+
+            bmResult.HotelName = hotelNameElement?.InnerText.Replace("\n", "");
         }
     }
 }
--- TG_Web_Extraction/Program.cs
+++ TG_Web_Extraction/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TG_Web_Extraction
 {
@@ -6,12 +10,61 @@ namespace TG_Web_Extraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HQ Application for Task 1 : Web Extraction...\n\n");
 
             HTML_Extractiom extractiom = new HTML_Extractiom();
-            var fileContents = extractiom.ReadHtmlFile();
 
-            extractiom.Extractinfo(fileContents);
+            string filePath = @"E:\Cloud Drives\OneDrive\Documents\HQ plus - Backend Development Assignment\HQ plus - Backend Development Assignment\Task 1\Kempinski Hotel Bristol Berlin, Germany - Booking.com.html";
+            var fileData = extractiom.ReadHtmlFile(filePath);
+
+            if (string.IsNullOrEmpty(fileData.Item1))
+            {
+                Console.WriteLine("No html content found...\nPlease restart the aplication and enter a valid file location.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("\nExtracting information from the html file...\n");
+            var extractedData = extractiom.Extractinfo(fileData.Item1);
+
+            Console.WriteLine("Extracted json...\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(extractedData);
+
+            Console.ResetColor();
+
+            if (!string.IsNullOrEmpty(extractedData))
+            {
+                Console.WriteLine("\nSaving json output.\n");
+
+                var outPutPath = fileData.Item2 + "\\ExtractedData.json";
+                File.WriteAllText(outPutPath, extractedData);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Output is saved at\n{outPutPath}");
+                Console.ResetColor();
+
+                Console.WriteLine("\nTrying to open the output folder in explorer.");
+                try
+                {
+                    Process.Start(fileData.Item2);
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    if (ex.Message.Contains("Access is denied"))
+                    {
+                        Console.WriteLine("Access is denied to open the folder in explorer.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to open the folder.");
+                    }
+                    Console.ResetColor();
+                }
+            }
 
             Console.Read();
         }
